76b9cfd1a060d8f0b40f7b1008d15e6c6315b7c2
VictordelaM <delamotte.v@gmail.com>
2024-03-18 14:26:02 +0100

    backend index

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import cors from 'cors'
+import multer from 'multer'
+import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+const PORT = 3000
+const app = express()
+const mult = multer()
+
+
+app.use(cors({origin: "http://localhost:5173"}))
+
+await mongoose.connect(process.env.MONGODB_URI)
+
+app.post("boote", mult.none(), async(req,res)=>{
+    booteInputData = req.body
+    const boot = new Boot(booteInputData)
+    await boot.save()
+    res.send()
+})
+
+
+app.listen(PORT)
